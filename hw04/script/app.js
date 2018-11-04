e322625dd5bdaf2f7bfe3506d5030b7e0f70c269
See3200 <meer3200@gmail.com>
2018-11-04 19:07:57 +0200

    added entire HW3

--- hw04/script/app.js
+++ hw04/script/app.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
 
-    console.log(1);
     $("#main-slider").slick({
         dots: false,
         arrows: false,
@@ -11,4 +10,28 @@ $( document ).ready(function() {
         slidesToScroll: 1,
     });
 
+    if ($('#go-top').length) {
+        console.log(1);
+        var target = $("#go-top");
+        var scrollTrigger = 100, // px
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    target.show(200);
+                } else {
+                    target.hide();
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        target.on('click', function (e) {
+            console.log(2);
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
 });
